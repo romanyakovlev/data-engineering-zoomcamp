7cfd93b6795a95c4bfd9cca9d727fc03b027be92
Roman Yakovlev <yakovlevroman8@gmail.com>
2023-05-04 01:03:53 +0400

    add Dockerfile

--- project/start.sh
+++
@@ -1,5 +0,0 @@
-export GOOGLE_APPLICATION_CREDENTIALS="/home/roman/Загрузки/sacred-alloy-375819-e7268563e1e4.json"
-terraform init
-terraform plan -var="project=sacred-alloy-375819"
-terraform apply -var="project=sacred-alloy-375819"
-prefect cloud login -k pnu_iKNmGcmslZs8wbN5xHXYwoRLAQPOF14GlPT3

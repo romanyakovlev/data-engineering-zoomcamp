37ec1b38842459f67295c45f202252ff2f0ccf95
Roman Yakovlev <yakovlevroman8@gmail.com>
2023-05-04 04:40:47 +0400

    fix chunks upload to local storage

--- project/4_deploy.sh
+++ project/4_deploy.sh
@@ -5,7 +5,7 @@ source spotify_project_venv/bin/activate
 # create prefect blocks
 python blocks/gcp_credentials.py
 python blocks/gcp_big_query.py
-python blocks/gcp_cloud_run.py
+python blocks/gcp_cloud_run_job.py
 python blocks/gcp_cloud_storage.py
 
 # deploy cloud run flow
--- project/flows/etl_web_to_gcs.py
+++ project/flows/etl_web_to_gcs.py
@@ -1,7 +1,7 @@
 import kaggle
 import pandas as pd
 from pathlib import Path
-from typing import List
+from typing import Generator, Tuple
 from prefect import flow, task
 from prefect.filesystems import GCS
 
@@ -22,37 +22,24 @@ def fetch_spotify_dataset(dataset: str, dir_path: str, dataset_path: str) -> Pat
 
 
 @task(log_prints=True)
-def write_gcs(path: Path) -> None:
+def write_gcs(df: pd.DataFrame, chunk_file_path: Path) -> None:
     """Upload local csv chunk file to GCS"""
-    with open(path, "r") as file:
-        csv_data = file.read()
-    gcs_block.write_path(path=path.as_posix(), content=csv_data.encode('utf-8'))
+    gcs_block.write_path(path=chunk_file_path.as_posix(), content=df.to_csv().encode("utf-8"))
+    print(f"Chunk {chunk_file_path.as_posix()} is uploaded to GCS.")
 
 
-@task(log_prints=True)
-def repartition_large_file(source_path: Path, chunksize: int = 1000000) -> List[Path]:
-    path_list = []
+def get_large_file_by_chunks(source_path: Path, chunksize: int = 1000000) -> Generator[Tuple[int, pd.DataFrame]]:
     Path(f"./data/chunks").mkdir(parents=True, exist_ok=True)
-    c = 0
     for i, chunk in enumerate(pd.read_csv(source_path, chunksize=chunksize)):
-        chunk_file_path = Path(f"./data/chunks/charts_{i}.csv")
-        chunk.to_csv(chunk_file_path)
-        path_list.append(chunk_file_path)
-        print(f"chunk {chunk_file_path} is created")
-        c += 1
-        if c == 5:
-            break
-    return path_list
+        yield i, chunk
 
 
 @task(log_prints=True)
-def clean(path: Path) -> None:
+def clean(df: pd.DataFrame) -> pd.DataFrame:
     """Fix dtype issues"""
-    df = pd.read_csv(path)
     df["streams"] = df["streams"].astype('Int64')
     df["date"] = pd.to_datetime(df["date"])
-    df.to_csv(path)
-    print(f"chunk {path.as_posix()} is cleaned")
+    return df
 
 
 @flow(log_prints=True, name="Subflow - Web to GCS step")
@@ -60,10 +47,10 @@ def etl_web_to_gcs() -> None:
     """The main ETL function"""
     dataset, dir_path, dataset_path = "dhruvildave/spotify-charts", "./data", "./data/spotify-charts.zip"
     path = fetch_spotify_dataset(dataset, dir_path, dataset_path)
-    chunk_path_list = repartition_large_file(path)
-    for chunk_path in chunk_path_list:
-        clean(chunk_path)
-        write_gcs(chunk_path)
+    for i, chunk_df in get_large_file_by_chunks(path):
+        cleaned_chunk = clean(chunk_df)
+        chunk_file_path = Path(f"./data/chunks/charts_{i}.csv")
+        write_gcs(cleaned_chunk, chunk_file_path)
 
 
 if __name__ == "__main__":

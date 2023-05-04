86097975224ae29a37204de96ed60b1ac55ab3d4
Roman Yakovlev <yakovlevroman8@gmail.com>
2023-05-04 04:10:22 +0400

    add scripts

--- project/1_init_infra.sh
+++ project/1_init_infra.sh
@@ -1,5 +1,5 @@
 export $(cat .env | xargs)
 
 terraform init
-terraform plan -var="project=$PROJECT_ID"
-terraform apply -var="project=$PROJECT_ID"
+terraform plan -var="project=$GCP_PROJECT_ID"
+terraform apply -var="project=$GCP_PROJECT_ID"
